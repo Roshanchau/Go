6ecb450714d9779d2f24dd35b65421c562240bae
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2025-05-28 19:45:02 +0545

    funcitons, loop , conditional if , defer in Go.

--- hello.go
+++ hello.go
@@ -6,6 +6,42 @@ import (
 	"rsc.io/quote"
 )
 
+type Person struct {
+	age int
+}
+
+func (x Person) Age() int {
+	return x.age
+}
+
 func main() {
 	fmt.Println(quote.Go())
+
+	x := 7
+	y := 2
+	fmt.Println(x<<8 + y<<16)
+
+	if x > 10 {
+		fmt.Println("y =>", y)
+	} else if x > 5 {
+		fmt.Printf("x is %d \n", x)
+	}
+
+	p := Person{age: 30}
+	fmt.Println("Person's age:", p.Age())
+
+	sum := 0
+	for i := range 10 {
+		sum += i
+	}
+	fmt.Println("Sum of first 10 integers:", sum)
+
+	numbers := []int{1, 20, 25}
+	for index, value := range numbers {
+		fmt.Printf("index: %d , value: %d \n", index, value)
+	}
+
+	defer fmt.Println("First defer")
+	defer fmt.Println("Second defer")
+	fmt.Println("Main body")
 }
